ed16d48f290ac7fc06fded8aa08f96fb2c09a0ec
Alan <alanmonteiro105@gmail.com>
2023-10-19 09:53:51 -0300

    projeto base kode3tech azure-actions

--- plugins/azure-actions-backend/src/index.ts
+++
@@ -1,5 +0,0 @@
-// export * from './service/router';
-export { createRouter } from './service/router';
-export { createPluginRemoveAzureReposAction } from './actions/deleteRepositories';
-export { createPluginRemoveAzurePipelinesAction } from './actions/deletePipelines';
-export { createPluginAddAzureVariableGroups } from './actions/addVariables';
